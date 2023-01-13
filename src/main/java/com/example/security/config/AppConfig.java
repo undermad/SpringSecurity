590b6e66b3aceb36bd640191438ef19edd749e47
Ectimel <dtworek94@gmail.com>
2023-01-13 03:34:41 +0000

    basic spring mvc connection with pure java configuration without any xml file!

---
+++ src/main/java/com/example/security/config/AppConfig.java
@@ -0,0 +1,25 @@
+package com.example.security.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.example.security")
+public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+
+        internalResourceViewResolver.setPrefix("/WEB-INF/view/");
+        internalResourceViewResolver.setSuffix(".jsp");
+
+        return internalResourceViewResolver;
+    }
+}
